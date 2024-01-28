1ea21c5b01728f2abc2ad6b04b27c8611c8b3a01
shiramoyal1 <157953322+shiramoyal1@users.noreply.github.com>
2024-01-28 11:49:23 +0200

    Update Shiur2.java

--- Shiur2.java
+++ Shiur2.java
@@ -1,1 +1,31 @@
+//q1
+public class Main {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 2;
+    int c = 5;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
 
+  }
+}
+
+//q2
+public class Main {
+  public static void main(String[] args) {
+    int x = 1;
+    double y = 1.2;
+    System.out.println(x + "   " + y);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    char a = 's';
+    char b = 'h';
+    char c = 'i';
+    char d = 'r';
+    char e = 'a';
+    System.out.println(a + "" + b + c + d + e);
+  }
