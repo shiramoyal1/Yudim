1eb6d3ad3567e51084a3232bfba3acd0cce0a233
shiramoyal1 <157953322+shiramoyal1@users.noreply.github.com>
2024-01-30 16:32:03 +0200

    Update shiur4 .java

--- shiur4 .java
+++ shiur4 .java
@@ -14,3 +14,25 @@ public class Main {
         System.out.print(name1.compareTo(name3));
         System.out.print(name3.compareTo(name2));
     }
+//q2
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a sentence till 4 words");
+        String str1 = scan.nextLine();
+        int size = str1.length();
+        System.out.print(size);
+    }
+}
+//q3
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter your word");
+        String str1 = scan.next();
+        if (str1.charAt(0) == str1.charAt(2))
+            System.out.print("the word is palindom");
+        else
+            System.out.print("the word is not polidrom");
