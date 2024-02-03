be66065c9751aa35938a34f40a5b9b37f511c814
shiramoyal1 <157953322+shiramoyal1@users.noreply.github.com>
2024-02-03 13:29:42 +0200

    Update Shiur2.java

--- Shiur2.java
+++ Shiur2.java
@@ -19,7 +19,7 @@ public class Main {
     System.out.println(x + "   " + y);
   }
 }
-
+//q3
 public class Main {
   public static void main(String[] args) {
     char a = 's';
@@ -29,3 +29,43 @@ public class Main {
     char e = 'a';
     System.out.println(a + "" + b + c + d + e);
   }
+//q4 
+public class Main {
+    public static void main(String[] args) {
+        int z=3;
+        int r=4;
+        System.out.print(z+ "is less than"+r);
+    }
+    }
+//q5
+public class Main {
+    public static void main(String[] args) {
+        int z = 3, x = 4, y = 5;
+        System.out.print("the avg number is" + ((x + z + y) / 3));
+    }
+}
+  //q6
+public class Main {
+    public static void main(String[] args) {
+        int a = 13, b = 12;
+        System.out.print("c before root equal =" + a * a + b * b);
+
+    }
+
+}
+  //q7
+public class Main {
+    public static void main(String[] args) {
+        int x = 3, y;
+        System.out.print("y=" + (y=(2 * x) + 5));
+    }
+}
+  //q8
+public class Main {
+    public static void main(String[] args) {
+        String sen1 = "I like to study computer science", sen2 = "and therefore ready to invest",
+                sen3 = "hours a day for practice";
+        int houres = 7;
+        System.out.print(sen1 + sen2 + houres + sen3);
+    }
+}
