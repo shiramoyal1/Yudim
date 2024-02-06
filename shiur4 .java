9fa5cf1e903a0b1a05892519530c6b70558a2284
shiramoyal1 <157953322+shiramoyal1@users.noreply.github.com>
2024-02-06 14:44:21 +0200

    Update shiur4 .java

--- shiur4 .java
+++ shiur4 .java
@@ -36,3 +36,81 @@ public class Main {
             System.out.print("the word is palindom");
         else
             System.out.print("the word is not polidrom");
+
+
+//q4
+        import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a string");
+        String str1 = scan.nextLine();
+        int inputString = 0;
+        String reversedString = new StringBuilder(inputString).reverse().toString();
+        System.out.println("the reverse str is" + reversedString);
+    }
+}
+//q5
+        System.out.print("Enter a four latters string: ");
+        String str = scanner.nextLine();
+        String correctedStr = "";
+        char firstChar = str.charAt(0);
+        if (firstChar == 'u' || firstChar == 'o' || firstChar == 'i' || firstChar == 'e' || firstChar == 'a')
+        {
+            correctedStr += '*';
+        }
+        else
+        {
+            correctedStr += firstChar;
+        }
+        char secondChar = str.charAt(1);
+        if (secondChar == 'u' || secondChar == 'o' || secondChar == 'i' || secondChar == 'e' || secondChar == 'a')
+        {
+            correctedStr += '*';
+        }
+        else
+        {
+            correctedStr += secondChar;
+        }
+        char thirdChar = str.charAt(2);
+        if (thirdChar == 'u' || thirdChar == 'o' || thirdChar == 'i' || thirdChar == 'e' || thirdChar == 'a')
+        {
+            correctedStr += '*';
+        }
+        else
+        {
+            correctedStr += thirdChar;
+        }
+        char fourthChar = str.charAt(3);
+        if (fourthChar == 'u' || fourthChar == 'o' || fourthChar == 'i' || fourthChar == 'e' || fourthChar == 'a')
+        {
+            correctedStr += '*';
+        }
+        else
+        {
+            correctedStr += fourthChar;
+        }
+        System.out.println("Corrected string: " + correctedStr);
+
+//q6
+        import java.util.Scanner;
+public class Main {
+    public static <scan> void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a num =<100");
+        int Num = scan.nextInt();
+        if (Num < 10) {
+            System.out.println("the number is a one digit number");
+        }
+        if (Num >= 10) {
+            System.out.println("the number is a two digit number");
+        }
+        if (Num%2==0)
+        {
+            System.out.println("shared at two");
+        }
+        if (Num%3==0)
+            System.out.println("shared at three");
+    }
+}
+
