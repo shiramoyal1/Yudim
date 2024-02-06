dc6080feaf683b3d044c66c59deec886e48129f8
shiramoyal1 <157953322+shiramoyal1@users.noreply.github.com>
2024-02-06 15:26:28 +0200

    Create Shiur5.java

---
+++ Shiur5.java
@@ -0,0 +1,10 @@
+//q1 import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter your name");
+        String name = scan.nextLine();
+        System.out.print("hello");
+        System.out.print(name);
+    }
+}
